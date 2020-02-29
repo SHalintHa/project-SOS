f9794ce3208a028a4b35bd4b2d27c201455ecd50
SHalintHa <subhashana011@gmail.com>
2020-02-29 12:59:11 +0530

    initial commit

---
+++ app/src/main/java/com/shalintha/sos/CustomLayoutListViewActivity.java
@@ -0,0 +1,14 @@
+package com.shalintha.sos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomLayoutListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_layout_list_view);
+    }
+}
